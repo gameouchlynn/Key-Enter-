36aadbf4abfe8f703e04595f2738f9acd20c4960
겜악린 <76154915+gameouchlynn@users.noreply.github.com>
2023-01-07 17:37:21 +0900

    Project

---
+++ ConsoleApplication3/ConsoleApplication3/game.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#pragma warning(disable:4996)
+int main(void)
+{
+	char a;
+	int score = 0;
+
+	while (1)
+	{
+		scanf("%c", &a);
+		score +=1;
+
+		printf("%d\n", score);
+		
+	}
+	
+}
